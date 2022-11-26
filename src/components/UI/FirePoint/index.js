5199769f5764100ce97aab6757af7cf5b5f99976
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2022-11-26 03:26:07 -0300

    ✨ feat: add load when clicking on fire

--- src/components/UI/FirePoint/index.js
+++ src/components/UI/FirePoint/index.js
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import MapboxGL from "@rnmapbox/maps";
 
 import IconSimple from "react-native-vector-icons/SimpleLineIcons";
 import { useTheme } from "styled-components";
-import { TouchableOpacity } from "react-native";
+import { TouchableOpacity, ActivityIndicator } from "react-native";
 
 const FirePoint = ({ register, setFireDetails }) => {
 
 	const theme = useTheme();
+	const [load, setLoad] = useState(false);
 
-	function showFireDetails() {
-		const copy = JSON.parse(JSON.stringify(register));
-		if (typeof copy.status === "string") {
-			copy.status = JSON.parse(copy.status);
+	useEffect(() => {
+		if (load) {
+			const copy = JSON.parse(JSON.stringify(register));
+			if (typeof copy.status === "string") {
+				copy.status = JSON.parse(copy.status);
+			}
+			setFireDetails(() => copy);
+			setLoad(false);
 		}
-		setFireDetails(() => copy);
-	}
+	}, [load]);
 
 	return (
 		<MapboxGL.MarkerView
@@ -27,26 +31,32 @@ const FirePoint = ({ register, setFireDetails }) => {
 		>
 			{register.userCreated ? (
 				<TouchableOpacity
-					onPress={() => showFireDetails()}
+					onPress={() => setLoad(true)}
 				>
-					<IconSimple
-						name='fire'
-						size={30}
-						color={theme.colors.icon["accent-color-v2"]}
-					/>
+					{load && <ActivityIndicator size="small" color="#F00000" />}
+					{!load && (
+						<IconSimple
+							name='fire'
+							size={30}
+							color={theme.colors.icon["accent-color-v2"]}
+						/>
+					)}
 				</TouchableOpacity>
 			) : (
 				<TouchableOpacity
-					onPress={() => showFireDetails()}
+					onPress={() => setLoad(true)}
 				>
-					<IconSimple
-						name='fire'
-						size={30}
-						color={register.brightness >= 500 ?
-							theme.colors.icon["accent-color-v1"] :
-							theme.colors.icon["accent-color-v3"]
-						}
-					/>
+					{load && <ActivityIndicator size="small" color="#F00000" />}
+					{!load && (
+						<IconSimple
+							name='fire'
+							size={30}
+							color={register.brightness >= 500 ?
+								theme.colors.icon["accent-color-v1"] :
+								theme.colors.icon["accent-color-v3"]
+							}
+						/>
+					)}
 				</TouchableOpacity>
 			)}
 		</MapboxGL.MarkerView>
